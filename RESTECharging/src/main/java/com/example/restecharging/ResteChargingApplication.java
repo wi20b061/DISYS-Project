ccf886b8fca842a18b59acb2c923cea532b45bf5
Fiona K <fiona5@gmx.at>
2022-06-22 14:29:19 +0200

    test commit - first change

--- RESTECharging/src/main/java/com/example/restecharging/ResteChargingApplication.java
+++ RESTECharging/src/main/java/com/example/restecharging/ResteChargingApplication.java
@@ -2,6 +2,7 @@ package com.example.restecharging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//test comment for commit
 
 @SpringBootApplication
 public class ResteChargingApplication {
