97b48b8878e2db409b3ef8c857f071e2bd1fa350
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-06-24 15:25:07 +0200

    files erstellt

---
+++ StationDataCollector/src/main/java/model/Charging.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Charging {
+    private int idstation;
+    private int idcustomer;
+    private int kwh;
+    private Timestamp datetime;
+}
